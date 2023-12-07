cf50157181d19699e29bddce3bd9bf0a7be4235d
Dominic <dominicezeifejafor@gmail.com>
2023-12-07 06:03:22 +0100

    project data file

--- src/mock/projectData.js
+++ src/mock/projectData.js
@@ -1,5 +1,22 @@
 
 // eslint-disable-next-line
 export default [
-    
+    {
+        projectName: '',
+        projectImg: '',
+        repoLink: 'https://www.github.com/',
+        demoLink: ''
+    },
+    {
+        projectName: '',
+        projectImg: '',
+        repoLink: 'https://www.github.com/',
+        demoLink: ''
+    },
+    {
+        projectName: '',
+        projectImg: '',
+        repoLink: 'https://www.github.com/',
+        demoLink: ''
+    }
 ]
