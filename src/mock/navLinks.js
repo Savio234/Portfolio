52adc98bf31b2b6dd703e4dc4164219ac3f34e35
Dominic <dominicezeifejafor@gmail.com>
2025-06-05 22:51:44 +0100

    routing

--- src/mock/navLinks.js
+++ src/mock/navLinks.js
@@ -14,6 +14,6 @@ export default [
     // },
     {
         name: 'Contact',
-        link: '/#contact'
+        link: '#contact'
     }
 ]
--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import { Footer, ProjectComponent } from '../components'
+import { Contact, Footer, ProjectComponent } from '../components'
 
 const Projects = () => {
   return (
     <>
       <ProjectComponent />
+      <Contact />
       <Footer />
     </>
   )
